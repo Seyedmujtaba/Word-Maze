7c1837ef520c48386ea7b257cca4acc3209e2e2c
Seyedmujtaba <seyedmujtaba.taba@gmail.com>
2025-12-27 22:29:58 +0330

    Add main function to initialize PyQt5 application

--- src/main.py
+++ src/main.py
@@ -1,1 +1,19 @@
+import sys
+from PyQt5.QtWidgets import QApplication
+
+from .main_window import create_main_window
+
+
+def main():
+    app = QApplication(sys.argv)
+
+    window = create_main_window(app)
+    window.showFullScreen()
+
+    sys.exit(app.exec_())
+
+
+if __name__ == "__main__":
+    main()
+
 
