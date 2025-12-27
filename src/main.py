f00714f249812d8e5021662c0e4585c54e354962
Seyedmujtaba <seyedmujtaba.taba@gmail.com>
2025-12-27 22:46:27 +0330

    Update main.py

--- src/main.py
+++ src/main.py
@@ -1,19 +1,4 @@
-import sys
-from PyQt5.QtWidgets import QApplication
-
-from .main_window import create_main_window
-
-
-def main():
-    app = QApplication(sys.argv)
-
-    window = create_main_window(app)
-    window.showFullScreen()
-
-    sys.exit(app.exec_())
-
+from .main_window import run
 
 if __name__ == "__main__":
-    main()
-
-
+    run()
