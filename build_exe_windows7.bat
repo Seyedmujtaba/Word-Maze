0494171b6de300494175cc3dfa259457225aa73f
Seyedmujtaba <seyedmujtaba.taba@gmail.com>
2025-12-28 23:04:19 +0330

    Delete build_exe_windows7.bat

--- build_exe_windows7.bat
+++
@@ -1,9 +0,0 @@
-py -3.8 -m PyInstaller ^
-  --onefile ^
-  --name WordMaze ^
-  --collect-all PyQt5 ^
-  --collect-submodules src ^
-  --hidden-import src.main ^
-  --add-data "data;data" ^
-  --add-data "assets;assets" ^
-  run_wordmaze.py
